41001f61052d88169d4cbec05b622fcc6b64fd9b
Sukhmani Kaur <sukhmanikaur.apppknit@gmail.com>
2020-03-22 23:56:53 +0530

    First Commit

---
+++ AppleMaps/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleMaps
+//
+//  Created by MACBOOK on 22/03/20.
+//  Copyright © 2020 SukhmaniKaur. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AppleMapsTests/AppleMapsTests.swift
@@ -0,0 +1,34 @@
+//
+//  AppleMapsTests.swift
+//  AppleMapsTests
+//
+//  Created by MACBOOK on 22/03/20.
+//  Copyright © 2020 SukhmaniKaur. All rights reserved.
+//
+
+import XCTest
+@testable import AppleMaps
+
+class AppleMapsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
